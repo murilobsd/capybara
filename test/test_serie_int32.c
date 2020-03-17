5c87c044ade12d72a3a612d999f4d1bb1a8edff4
murilo ijanc <mbsd@m0x.ru>
2020-03-16 22:22:58 -0300

    add new testing to set_name function

--- test/test_serie_int32.c
+++ test/test_serie_int32.c
@@ -1,8 +1,14 @@
-#include "unity.h"
+#include <string.h>
 
+#include "unity.h"
 #include "capybara/serie_int32.h"
 
-void test_new_serie_int32(void) {
+void setUp (void) {}
+void tearDown (void) {}
+
+void
+test_new_serie_int32(void)
+{
   	serie_int32_t *s = serie_int32_new();
 
 	TEST_ASSERT_NOT_NULL(s);
@@ -10,11 +16,21 @@ void test_new_serie_int32(void) {
 	s->ops->free_serie(s);
 }
 
-void setUp (void) {}
-void tearDown (void) {}
+void
+test_set_name_serie_int32(void)
+{
+  	serie_int32_t *s = serie_int32_new();
+	s->ops->set_name(s, "serie");
+
+	TEST_ASSERT_EQUAL_STRING("serie", s->ops->get_name(s));
+	TEST_ASSERT_EQUAL_STRING_LEN("serie", s->ops->get_name(s),
+	strlen("serie"));
+	s->ops->free_serie(s);
+}
 
 int main() {
   	UNITY_BEGIN();
   	RUN_TEST(test_new_serie_int32);
+  	RUN_TEST(test_set_name_serie_int32);
   	return UNITY_END();
 }
