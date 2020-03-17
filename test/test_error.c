f27637b06ac82533128edbb102ac4d939ed2521a
murilo ijanc <mbsd@m0x.ru>
2020-03-17 12:28:02 -0300

    testing function return error string

--- test/test_error.c
+++ test/test_error.c
@@ -10,21 +10,49 @@ void
 tearDown(void) {}
 
 capybara_error_t
-func_return_no_error(void)
+func_return_error_ok(void)
 {
 	return CAPY_ERROR_OK;
+}
+
+capybara_error_t
+func_return_error_nomem(void)
+{
+	return CAPY_ERROR_NOMEM;
 }
 
 void
 test_error_ok(void)
 {
-	TEST_ASSERT_EQUAL(CAPY_ERROR_OK, func_return_no_error());
+	TEST_ASSERT_EQUAL(CAPY_ERROR_OK, func_return_error_ok());
+}
+
+void
+test_error_nomem(void)
+{
+	TEST_ASSERT_EQUAL(CAPY_ERROR_NOMEM, func_return_error_nomem());
+}
+
+void
+test_error_ok_str(void)
+{
+	capybara_error_t err;
+
+	err = func_return_error_ok();
+
+	TEST_ASSERT_EQUAL_STRING("CAPY_ERROR_OK", capy_error_str(err));
+	TEST_ASSERT_EQUAL_STRING_LEN("CAPY_ERROR_OK", capy_error_str(err),
+		strlen("CAPY_ERROR_OK"));
 }
 
 int
 main(void)
 {
 	UNITY_BEGIN();
+
 	RUN_TEST(test_error_ok);
+	RUN_TEST(test_error_nomem);
+	RUN_TEST(test_error_ok_str);
+
 	return UNITY_END();
 }
